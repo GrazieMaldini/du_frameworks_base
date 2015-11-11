e9cdf0571dcaa8dc30c1b1232252130c571b3e14
Roman Birg <roman@cyngn.com>
2015-11-11 15:01:41 -0500

    Add Quick Settings Pulldown options [1/2]
    PureNexus Edits:
    *add always quick pulldown option
    *fix for double finger pulldown

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5037,6 +5037,12 @@ public final class Settings {
                 "status_bar_locked_on_secure_keyguard";
 
         /**
+         * Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
