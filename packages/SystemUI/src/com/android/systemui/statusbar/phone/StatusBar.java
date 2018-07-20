71e2756c70003cbd4ad3585d019b513fc34712e2
ElDainosor <eldainosor@gmail.com>
2018-07-20 03:02:27 -0300

    SystemUI: Ambient Visualizer [1/2]
    This is based on LOS Lockscreen Visualizer and well, this adds the posibility to show the visualizer on Ambient display
    Normally it will show it with a 25% of opacity because of a aesthetic reason, instead of fully opaque and being an annoyance and feel out of touch with the ambient display.
    Note that this may have issues sometimes maybe and also, the idea was to make it white on ambient and colored on when lockscreen shows up and maybe there will be some code leftovers but well, maybe in a future
    
    Bootleggers (eldainosor) pie edits: Adapt it for pie/tuner switch
    
    Change-Id: If14a5e20e4075282a96f19ec7f16fafe8dc704a5
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -538,6 +538,10 @@ public class StatusBar extends SystemUI implements DemoMode,
     private VisualizerView mVisualizerView;
 
     private boolean mWallpaperSupportsAmbientMode;
+
+    // LS visualizer on Ambient Display
+    private boolean mAmbientVisualizer;
+
     private final BroadcastReceiver mWallpaperChangedReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -1973,6 +1977,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.SHOW_BACK_ARROW_GESTURE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.Secure.AMBIENT_VISUALIZER_ENABLED),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -1992,6 +1999,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.Secure.getUriFor(
                     Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS))) {
                 setFpToDismissNotifications();
+            } else if (uri.equals(Settings.Secure.getUriFor(
+                    Settings.Secure.AMBIENT_VISUALIZER_ENABLED))) {
+                setAmbientVis();
             }
             update();
         }
@@ -2006,6 +2016,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         setPulseOnNewTracks();
         setFpToDismissNotifications();
         setHideArrowForBackGesture();
+        setAmbientVis();
         updateCorners();
         }
     }
@@ -2070,6 +2081,12 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (getNavigationBarView() != null) {
             getNavigationBarView().updateBackArrowForGesture();
         }
+    }
+
+    private void setAmbientVis() {
+        mAmbientVisualizer = Settings.Secure.getIntForUser(
+                mContext.getContentResolver(), Settings.Secure.AMBIENT_VISUALIZER_ENABLED, 0,
+                UserHandle.USER_CURRENT) == 1;
     }
 
     /**
@@ -3938,6 +3955,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
 
         mStatusBarStateController.setIsDozing(dozing);
+        if (mAmbientVisualizer && mDozing) {
+            mVisualizerView.setVisible(true);
+        }
     }
 
     private void updateKeyguardState() {
