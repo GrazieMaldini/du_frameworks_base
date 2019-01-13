b99e3443d4dc894785126bd9a62f9f3ccc763bd1
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/com/android/internal/util/du/Utils.java
+++ core/java/com/android/internal/util/du/Utils.java
@@ -42,6 +42,7 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
+import android.util.DisplayMetrics;
 
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
@@ -225,5 +226,23 @@ public class Utils {
             }
         } catch (RemoteException e) {
         }
+    }
+
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
     }
 }
