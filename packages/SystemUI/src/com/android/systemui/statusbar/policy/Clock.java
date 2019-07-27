6a1e87c10de4d366dc90e95400eb1dddec4e2699
Genkzsz11 <genkzsz11@gmail.com>
2019-07-27 16:06:36 +0700

    Revert "Statusbar clock: restore qsheader check within a saved instance"
    This reverts commit ac4eb68ce39f6c19ffdb61b6f4f538c46c5050d4.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -77,7 +77,6 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
     private static final String VISIBLE_BY_POLICY = "visible_by_policy";
     private static final String VISIBLE_BY_USER = "visible_by_user";
     private static final String VISIBILITY = "visibility";
-    private static final String QSHEADER = "qsheader";
 
     private final CurrentUserTracker mCurrentUserTracker;
     private int mCurrentUserId;
@@ -165,7 +164,6 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         bundle.putBoolean(VISIBLE_BY_POLICY, mClockVisibleByPolicy);
         bundle.putBoolean(VISIBLE_BY_USER, mClockVisibleByUser);
         bundle.putInt(VISIBILITY, getVisibility());
-        bundle.putBoolean(QSHEADER, mQsHeader);
 
         return bundle;
     }
@@ -188,7 +186,6 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         if (bundle.containsKey(VISIBILITY)) {
             setVisibility(bundle.getInt(VISIBILITY));
         }
-        mQsHeader = bundle.getBoolean(QSHEADER, false);
     }
 
     @Override
