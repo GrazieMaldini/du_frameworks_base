2a98ddf2b6f5bdfaeb61b1d52a89ff84c8d10b0f
Genkzsz11 <Genkzsz11@gmail.com>
2019-10-24 08:26:51 +0700

    Revert "Custom Rounded Corner and Padding preferences [1/2]"
    This reverts commit dc1c59259678ebd029937c2cc02787127fc1275a.
    
    Change-Id: If8f1b60daefdf1ea81ee1faf04981406190876f1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4870,18 +4870,6 @@ public final class Settings {
         public static final Validator STATUSBAR_CLOCK_DATE_POSITION_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
-         * Setting to allow setting rounded corner size and content padding
-         */
-        public static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
-        public static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
-
-        /**
-         * Setting to disable rounded corner preferences and use frameworks values instead
-         * @hide
-         */
-        public static final String SYSUI_ROUNDED_FWVALS = "sysui_rounded_fwvals";
-
-        /**
          * Whether to display set of animations when prompting the power menu
          *
          * @hide
