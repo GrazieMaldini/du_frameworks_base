omnirom/android_frameworks_base_old

SystemUI: Fix disable QS pulldown on secure lockscreens
Change-Id: Ide95a72ff7b271476a8b3f8fbf163db8b7187b01

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -817,7 +817,7 @@ public class NotificationPanelView extends PanelView implements
     }
 
     public void setQsExpansionEnabled(boolean qsExpansionEnabled) {
-        mQsExpansionEnabled = qsExpansionEnabled && !isQsSecureExpandDisabled();
+        mQsExpansionEnabled = qsExpansionEnabled;
         if (mQs == null) return;
         mQs.setHeaderClickable(qsExpansionEnabled);
     }
@@ -879,7 +879,7 @@ public class NotificationPanelView extends PanelView implements
     }
 
     public void expandWithQs() {
-        if (mQsExpansionEnabled) {
+        if (mQsExpansionEnabled && !isQsSecureExpandDisabled()) {
             mQsExpandImmediate = true;
             mNotificationStackScroller.setShouldShowShelfOnly(true);
         }
@@ -1155,7 +1155,7 @@ public class NotificationPanelView extends PanelView implements
         final int action = event.getActionMasked();
         if (action == MotionEvent.ACTION_DOWN && getExpandedFraction() == 1f
                 && mBarState != StatusBarState.KEYGUARD && !mQsExpanded
-                && mQsExpansionEnabled) {
+                && mQsExpansionEnabled && !isQsSecureExpandDisabled()) {
 
             // Down in the empty area while fully expanded - go to QS.
             mQsTracking = true;
@@ -1344,7 +1344,7 @@ public class NotificationPanelView extends PanelView implements
     @Override
     public void onOverscrollTopChanged(float amount, boolean isRubberbanded) {
         cancelQsAnimation();
-        if (!mQsExpansionEnabled) {
+        if (!mQsExpansionEnabled || isQsSecureExpandDisabled()) {
             amount = 0f;
         }
         float rounded = amount >= 1f ? amount : 0f;
@@ -1830,7 +1830,7 @@ public class NotificationPanelView extends PanelView implements
      * @return Whether we should intercept a gesture to open Quick Settings.
      */
     private boolean shouldQuickSettingsIntercept(float x, float y, float yDiff) {
-        if (!mQsExpansionEnabled || mCollapsedOnDown) {
+        if (!mQsExpansionEnabled || mCollapsedOnDown || isQsSecureExpandDisabled()) {
             return false;
         }
         View header = mKeyguardShowing || mQs == null ? mKeyguardStatusBar : mQs.getHeader();

GrazieMaldini/du_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -904,7 +904,7 @@ public class NotificationPanelView extends PanelView implements
     }
 
     public void setQsExpansionEnabled(boolean qsExpansionEnabled) {
-        mQsExpansionEnabled = qsExpansionEnabled && !isQSEventBlocked();
+        mQsExpansionEnabled = qsExpansionEnabled;
         if (mQs == null) return;
         mQs.setHeaderClickable(mQsExpansionEnabled);
     }
@@ -966,7 +966,7 @@ public class NotificationPanelView extends PanelView implements
     }
 
     public void expandWithQs() {
-        if (mQsExpansionEnabled) {
+        if (mQsExpansionEnabled && !isQSEventBlocked()) {
             mQsExpandImmediate = true;
             mNotificationStackScroller.setShouldShowShelfOnly(true);
         }
@@ -1275,7 +1275,7 @@ public class NotificationPanelView extends PanelView implements
         final int action = event.getActionMasked();
         if (action == MotionEvent.ACTION_DOWN && getExpandedFraction() == 1f
                 && mBarState != StatusBarState.KEYGUARD && !mQsExpanded
-                && mQsExpansionEnabled) {
+                && mQsExpansionEnabled && !isQSEventBlocked()) {
 
             // Down in the empty area while fully expanded - go to QS.
             mQsTracking = true;
@@ -1511,7 +1511,7 @@ public class NotificationPanelView extends PanelView implements
     @Override
     public void onOverscrollTopChanged(float amount, boolean isRubberbanded) {
         cancelQsAnimation();
-        if (!mQsExpansionEnabled) {
+        if (!mQsExpansionEnabled || isQSEventBlocked()) {
             amount = 0f;
         }
         float rounded = amount >= 1f ? amount : 0f;
@@ -2046,7 +2046,7 @@ public class NotificationPanelView extends PanelView implements
      */
     private boolean shouldQuickSettingsIntercept(float x, float y, float yDiff) {
         if (!mQsExpansionEnabled || mCollapsedOnDown
-                || (mKeyguardShowing && mKeyguardBypassController.getBypassEnabled())) {
+                || (mKeyguardShowing && mKeyguardBypassController.getBypassEnabled() || isQSEventBlocked())) {
             return false;
         }
         View header = mKeyguardShowing || mQs == null ? mKeyguardStatusBar : mQs.getHeader();