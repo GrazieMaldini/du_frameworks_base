97e06d708964b555eba155235d6a16baea5d457f
Pranav Vashi <neobuddy89@gmail.com>
2020-01-16 00:37:03 +0530

    Bring back ticker for music ticker and Android 10 fixes
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -475,9 +475,9 @@ public class StatusBar extends SystemUI implements DemoMode,
     private boolean mBrightnessChanged;
     private boolean mJustPeeked;
 
-   // status bar notification ticker
-    private int mTickerEnabled;
-    private Ticker mTicker;
+    // status bar notification ticker
+    public int mTickerEnabled;
+    public Ticker mTicker;
     private boolean mTicking;
     private int mTickerAnimationMode;
     private int mTickerTickDuration;
@@ -513,6 +513,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             mLinger = BRIGHTNESS_CONTROL_LINGER_THRESHOLD + 1;
         }
     };
+
+    public void resetTrackInfo() {
+        if (mTicker != null) {
+            mTicker.resetShownMediaMetadata();
+        }
+    }
 
     // ensure quick settings is disabled until the current user makes it through the setup wizard
     @VisibleForTesting
@@ -734,6 +740,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mLockscreenUserManager = Dependency.get(NotificationLockscreenUserManager.class);
         mGutsManager = Dependency.get(NotificationGutsManager.class);
         mMediaManager = Dependency.get(NotificationMediaManager.class);
+        mMediaManager.addCallback(this);
         mEntryManager = Dependency.get(NotificationEntryManager.class);
         mBypassHeadsUpNotifier.setUp(mEntryManager);
         mNotificationInterruptionStateProvider =
@@ -1267,6 +1274,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mHeadsUpManager, mStatusBarWindow, mStackScroller, mDozeScrimController,
                 mScrimController, mActivityLaunchAnimator, mDynamicPrivacyController,
                 mNotificationAlertingManager, rowBinder);
+        mPresenter.addCallback(this);
 
         mNotificationListController =
                 new NotificationListController(
