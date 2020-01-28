Dabug123/android_frameworks_base-2

Partial screenshot with gestures nav: don't break it on BACK action
we can now draw our partial screenshot rectangle starting the swipe from
left or right side of the screen

ps: if you wanna block the back action only from the left side, just
use the mIsOnLeftEdge bool check in EdgeBackGestureHandler.java
actually i think it's just handy to block both sides

ps2: this commit adds a system api to know the partial screenshot state
so it can be eventually used for other purposes

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -83,6 +83,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.internal.messages.nano.SystemMessageProto.SystemMessage;
+import com.android.internal.util.du.ActionUtils;
 import com.android.systemui.R;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.SystemUI;
@@ -631,6 +632,7 @@ class GlobalScreenshot {
     void takeScreenshotPartial(final Runnable finisher, final boolean statusBarVisible,
             final boolean navBarVisible) {
         mWindowManager.addView(mScreenshotLayout, mWindowLayoutParams);
+        ActionUtils.setPartialScreenshot(true);
         mScreenshotSelectorView.setSelectionListener(
                 new ScreenshotSelectorView.OnSelectionListener() {
             @Override
@@ -679,6 +681,7 @@ class GlobalScreenshot {
     }
 
     void hideScreenshotSelector() {
+        ActionUtils.setPartialScreenshot(false);
         mWindowManager.removeView(mScreenshotLayout);
         mScreenshotSelectorView.stopSelection();
         mScreenshotSelectorView.setVisibility(View.GONE);
@@ -697,6 +700,8 @@ class GlobalScreenshot {
             } catch (IllegalArgumentException ignored) {
             }
         }
+        // called when unbinding screenshot service
+        ActionUtils.setPartialScreenshot(false);
     }
 
     /**

GrazieMaldini/du_frameworks_base

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -87,6 +87,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.internal.messages.nano.SystemMessageProto.SystemMessage;
+import com.android.internal.util.du.Utils;
 import com.android.systemui.R;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.SystemUI;
@@ -106,8 +107,6 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-
-
 
 /**
  * POD used in the AsyncTask which saves an image in the background.
@@ -674,6 +673,7 @@ class GlobalScreenshot {
     void takeScreenshotPartial(final Runnable finisher, final boolean statusBarVisible,
             final boolean navBarVisible) {
         mWindowManager.addView(mScreenshotLayout, mWindowLayoutParams);
+        Utils.setPartialScreenshot(true);
         mScreenshotSelectorView.setSelectionListener(
                 new ScreenshotSelectorView.OnSelectionListener() {
             @Override
@@ -722,6 +722,7 @@ class GlobalScreenshot {
     }
 
     void hideScreenshotSelector() {
+        Utils.setPartialScreenshot(false);
         mWindowManager.removeView(mScreenshotLayout);
         mScreenshotSelectorView.stopSelection();
         mScreenshotSelectorView.setVisibility(View.GONE);
@@ -740,6 +741,8 @@ class GlobalScreenshot {
             } catch (IllegalArgumentException ignored) {
             }
         }
+        // called when unbinding screenshot service
+        Utils.setPartialScreenshot(false);
     }
 
     /**