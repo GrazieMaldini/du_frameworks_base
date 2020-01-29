DirtyUnicorns/android_frameworks_base

Introduce Theme tile
Long press tile to change between Style (dark/light/auto ui)
mode and Accent (#FruityPebbles) mode.
Click to show detail view.

Change-Id: Ic9ca2a3305b4b79120d7ffca607477636fbbb5c5

packages/SystemUI/src/com/android/systemui/qs/QSDetailItemsList.java
@@ -113,9 +113,20 @@ public class QSDetailItemsList extends LinearLayout {
             view.setClickable(false); // let list view handle this
 
             final QSDetailItems.Item item = getItem(position);
+            if (item.doDisableFocus) {
+                view.setFocusable(false);
+            }
 
             final ImageView iv = (ImageView) view.findViewById(android.R.id.icon);
-            iv.setImageResource(item.icon);
+            if (item.doDisableTint) {
+                iv.setColorFilter(null);
+                iv.setImageTintList(null);
+            }
+            if (item.iconDrawable != null) {
+                iv.setImageDrawable(item.iconDrawable.getDrawable(iv.getContext()));
+            } else {
+                iv.setImageResource(item.icon);
+            }
             iv.getOverlay().clear();
             if (item.overlay != null) {
                 item.overlay.setBounds(0, 0, item.overlay.getIntrinsicWidth(),

GrazieMaldini/du_frameworks_base

 Conflicts:
	packages/SystemUI/res/values/du_strings.xml
	packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
Change-Id: Ifa1e1cb6f92afbfa2fd2839b1c7586a06be5c339
Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/QSDetailItemsList.java
@@ -113,9 +113,20 @@ public class QSDetailItemsList extends LinearLayout {
             view.setClickable(false); // let list view handle this
 
             final QSDetailItems.Item item = getItem(position);
+            if (item.doDisableFocus) {
+                view.setFocusable(false);
+            }
 
             final ImageView iv = (ImageView) view.findViewById(android.R.id.icon);
-            iv.setImageResource(item.icon);
+            if (item.doDisableTint) {
+                iv.setColorFilter(null);
+                iv.setImageTintList(null);
+            }
+            if (item.icon != null) {
+                iv.setImageDrawable(item.icon.getDrawable(iv.getContext()));
+            } else {
+                iv.setImageResource(item.iconResId);
+            }
             iv.getOverlay().clear();
             if (item.overlay != null) {
                 item.overlay.setBounds(0, 0, item.overlay.getIntrinsicWidth(),