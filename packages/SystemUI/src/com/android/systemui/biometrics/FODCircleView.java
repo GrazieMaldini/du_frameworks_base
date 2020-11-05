e2c1c3621c60e38ffe26dfaeec8cc571e46927ac
AdarshGrewal <adarshgrewal@gmail.com>
2020-11-05 17:39:05 +0000

    Revert "FODCircleView: Hide fod while going to sleep"
    This commit Partially Reverts https://review.lineageos.org/c/LineageOS/android_frameworks_base/+/286668/24..25
    	reason for revert:
    	breaks fodIcon on under certain conditions
    	* switching from LS to AOD using seamless transition
    	* powering screen on/off quickly
    
    Change-Id: Ic9419cb3d05bb78a2780c55c19566f6f84582711
    Signed-off-by: AdarshGrewal <adarshgrewal@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -151,7 +151,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         }
 
         @Override
-        public void onStartedGoingToSleep(int why) {
+        public void onScreenTurnedOff() {
             hide();
         }
 
