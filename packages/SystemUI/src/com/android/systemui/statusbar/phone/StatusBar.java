6471102abd045a0fb0afec5506faeb7ea0376ce9
ezio84 <brabus84@gmail.com>
2020-11-12 22:06:29 +0100

    ActionUtils: Reverse actions for QS and notification panel
    This just makes sense, so lets do it!
    
    - Update method names in PWM
    
    Change-Id: Ic08bee12c90dc55265dea3c3972f4087b93d2826
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2182,6 +2182,15 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     @Override
+    public void toggleSettingsPanel() {
+        if (mPanelExpanded) {
+            mShadeController.animateCollapsePanels();
+        } else {
+            animateExpandSettingsPanel(null);
+        }
+    }
+
+    @Override
     public void animateCollapsePanels(int flags, boolean force) {
         mShadeController.animateCollapsePanels(flags, force, false /* delayed */,
                 1.0f /* speedUpFactor */);
