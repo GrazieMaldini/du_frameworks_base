954bcb64b29d078332c6e46e7ee810f09f42f0d2
ezio84 <brabus84@gmail.com>
2020-11-15 09:45:01 +0100

    Essential only mode (aka Less boring heads up) [1/2]
    Allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    Change-Id: I9ce517a5974b1567339bc7d7c84ee35bb89c774b
    
    ---
    
    Less boring headsup: allow any sms/dialer app set as default by user
    thanks GeoZac for the suggestion!
    
    Change-Id: Ie7a725bd0fc579462b736f2e8520d59d14f402f2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5042,6 +5042,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+         * Whether to show heads up only for dialer and sms apps
+         * @hide
+         */
+        public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
