bef02f8cb7756a268c041833d917d0f14a929ea4
spezi77 <spezi7713@gmx.net>
2021-01-28 11:36:00 +0100

    SystemUI: Add a switch to prefer three QS tile rows v2.
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I4f83058661f3e1e1763be2178fbaf408c4ad751c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5909,6 +5909,11 @@ public final class Settings {
         public static final String OMNI_QS_MEDIA_PLAYER = "qs_media_player";
 
         /**
+         * @hide
+         */
+        public static final String OMNI_QS_LAYOUT_PREF_THREE_ROWS = "qs_layout_pref_three_rows";
+
+        /**
          * SettingsBackupAgent will combine its list with this so we dont need
          * to add new things into SettingsProvider SystemSettings
          * @hide
@@ -5920,6 +5925,7 @@ public final class Settings {
             OMNI_QS_QUICKBAR_COLUMNS,
             OMNI_QS_TILE_TITLE_VISIBILITY,
             OMNI_QS_MEDIA_PLAYER,
+            OMNI_QS_LAYOUT_PREF_THREE_ROWS,
         };
 
         /**
@@ -5940,6 +5946,7 @@ public final class Settings {
             OMNI_SETTINGS_VALIDATORS.put(OMNI_QS_LAYOUT_COLUMNS, 1);
             OMNI_SETTINGS_VALIDATORS.put(OMNI_QS_TILE_TITLE_VISIBILITY, 0);
             OMNI_SETTINGS_VALIDATORS.put(OMNI_QS_MEDIA_PLAYER, 0);
+            OMNI_SETTINGS_VALIDATORS.put(OMNI_QS_LAYOUT_PREF_THREE_ROWS, 0);
        }
     }
 
