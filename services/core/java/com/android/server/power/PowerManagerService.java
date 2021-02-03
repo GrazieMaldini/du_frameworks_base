2622a655d73f265b45d6936def81cdf3e99298ca
Pranav Vashi <neobuddy89@gmail.com>
2021-02-04 02:57:34 +0530

    SmartCharging: Clean up redundant settings define
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iae61900dcd6442776e03bd28c69dbb5dd05a9300

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1255,8 +1255,6 @@ public final class PowerManagerService extends SystemService
                 com.android.internal.R.string.config_SmartChargingSupspendValue);
         mPowerInputResumeValue = resources.getString(
                 com.android.internal.R.string.config_SmartChargingResumeValue);
-        mSmartChargingResetStats = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SMART_CHARGING_RESET_STATS, 0) == 1;
     }
 
     private void updateSettingsLocked() {
