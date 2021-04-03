AOSiP/platform_frameworks_base

ActionUtils: Add action to start Assistant
Change-Id: Ie1ea7d9ed014f27a0096d574af488ad99cdfba38

core/java/com/android/internal/util/custom/LineageButtons.java
@@ -33,6 +33,8 @@ import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 
+import com.android.internal.util.aosip.ActionUtils;
+
 import java.util.List;
 
 public final class LineageButtons {

GrazieMaldini/du_frameworks_base

core/java/com/android/internal/util/banana/LineageButtons.java
@@ -33,6 +33,8 @@ import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 
+import com.android.internal.util.banana.ActionUtils;
+
 import java.util.List;
 
 public final class LineageButtons {