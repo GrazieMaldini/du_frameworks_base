Havoc-OS/android_frameworks_base

base: Elmyra app: check proximity before triggering, and add more actions [2/2]
NB: override <bool name="config_has_elmyra">false</bool> to true in
device tree

part 1 is in Elmyra app tree
Change-Id: I8ff1478fcee0b10ab3bd6f5a84dc9b2245fb1171

Change-Id: Id2b644ebb2df18ab7e761ef53288e68efb79ab16
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

core/java/com/android/internal/util/custom/LineageButtons.java
@@ -33,6 +33,8 @@ import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 
+import com.android.internal.util.custom.Utils;
+
 import java.util.List;
 
 public final class LineageButtons {
@@ -206,6 +208,76 @@ public final class LineageButtons {
             mVolBtnMusicControls = Settings.System.getIntForUser(
                     resolver, Settings.System.VOLUME_BUTTON_MUSIC_CONTROL, 1,
                     UserHandle.USER_CURRENT) == 1;
+        }
+    }
+
+    // Elmyra app actions
+    public void performTriggeredAction(String action, Context context, boolean interactive) {
+        switch(action) {
+            case "flashlight":
+                Utils.toggleCameraFlash(false);
+                break;
+            case "assist":
+                Utils.startAssist();
+                break;
+            case "screenshot":
+                // already disabled when screen is OFF by Elmyra app
+                Utils.takeScreenshot(true/*full*/);
+                break;
+            case "partialscreenshot":
+                // already disabled when screen is OFF by Elmyra app
+                Utils.takeScreenshot(false/*full*/);
+                break;
+            case "camera":
+                if (!interactive) {
+                    Utils.switchScreenOff(context);
+                }
+                Utils.launchCamera(context);
+                break;
+            case "mute":
+                Utils.triggerHushMute(context);
+                break;
+            case "screen":
+                if (interactive) {
+                    Utils.switchScreenOff(context);
+                } else {
+                    Utils.switchScreenOn(context);
+                }
+                break;
+            case "skiptrack":
+                skipTrack();
+                break;
+            case "previoustrack":
+                previousTrack();
+                break;
+            case "voicesearch":
+                Utils.launchVoiceSearch(context);
+                break;
+            case "volumepanel":
+                // already disabled when screen is OFF by Elmyra app
+                Utils.toggleVolumePanel(context);
+                break;
+            case "powermenu":
+                // already disabled when screen is OFF by Elmyra app
+                Utils.showPowerMenu();
+                break;
+            case "toggleqspanel":
+                // already disabled when screen is OFF by Elmyra app
+                Utils.toggleQsPanel();
+                break;
+            case "togglenotificationspanel":
+                // already disabled when screen is OFF by Elmyra app
+                Utils.toggleNotifications();
+                break;
+            case "clearallnotifications":
+                // already disabled when screen is OFF by Elmyra app
+                Utils.clearAllNotifications();
+                break;
+            case "toggleringermodes":
+                Utils.toggleRingerModes(context);
+                break;
+            default:
+                break;
         }
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -133,6 +133,7 @@ import com.android.internal.logging.UiEventLoggerImpl;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.RegisterStatusBarResult;
+import com.android.internal.util.custom.LineageButtons;
 import com.android.internal.view.AppearanceRegion;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
@@ -2124,8 +2125,9 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     @Override
-    public void toggleCameraFlash() {
-        if (!isScreenFullyOff() && mDeviceInteractive && !isPulsing() && !mDozing) {
+    public void toggleCameraFlash(boolean proximityCheck) {
+        if (!proximityCheck ||
+                (!isScreenFullyOff() && mDeviceInteractive && !isPulsing() && !mDozing)) {
             toggleFlashlight();
             return;
         }
@@ -2139,6 +2141,22 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
             }
         }
+    }
+
+    // Elmyra app actions
+    @Override
+    public void triggerElmyraAction(String action) {
+        if (!isScreenFullyOff() && mDeviceInteractive && !isPulsing() && !mDozing) {
+            performTriggeredAction(action);
+            return;
+        }
+        mDozeServiceHost.triggerActionProximityCheck(action);
+    }
+
+    // Elmyra app actions
+    public void performTriggeredAction(String action) {
+        mVibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK));
+        LineageButtons.getAttachedInstance(mContext).performTriggeredAction(action, mContext, mDeviceInteractive);
     }
 
     void makeExpandedVisible(boolean force) {

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -779,7 +779,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void toggleFlashLight() {
         performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Flashlight toggle");
-        Utils.toggleCameraFlash();
+        Utils.toggleCameraFlash(true/*proximity check*/);
     }
 
     private UEventObserver mHDMIObserver = new UEventObserver() {

GrazieMaldini/du_frameworks_base

core/java/com/android/internal/util/banana/LineageButtons.java
@@ -33,6 +33,8 @@ import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 
+import com.android.internal.util.banana.bananaUtils;
+
 import java.util.List;
 
 public final class LineageButtons {
@@ -206,6 +208,76 @@ public final class LineageButtons {
             mVolBtnMusicControls = Settings.System.getIntForUser(
                     resolver, Settings.System.VOLUME_BUTTON_MUSIC_CONTROL, 1,
                     UserHandle.USER_CURRENT) == 1;
+        }
+    }
+
+    // Elmyra app actions
+    public void performTriggeredAction(String action, Context context, boolean interactive) {
+        switch(action) {
+            case "flashlight":
+                bananaUtils.toggleCameraFlash(false);
+                break;
+            case "assist":
+                bananaUtils.startAssist();
+                break;
+            case "screenshot":
+                // already disabled when screen is OFF by Elmyra app
+                bananaUtils.takeScreenshot(true/*full*/);
+                break;
+            case "partialscreenshot":
+                // already disabled when screen is OFF by Elmyra app
+                bananaUtils.takeScreenshot(false/*full*/);
+                break;
+            case "camera":
+                if (!interactive) {
+                    bananaUtils.switchScreenOff(context);
+                }
+                bananaUtils.launchCamera(context);
+                break;
+            case "mute":
+                bananaUtils.triggerHushMute(context);
+                break;
+            case "screen":
+                if (interactive) {
+                    bananaUtils.switchScreenOff(context);
+                } else {
+                    bananaUtils.switchScreenOn(context);
+                }
+                break;
+            case "skiptrack":
+                skipTrack();
+                break;
+            case "previoustrack":
+                previousTrack();
+                break;
+            case "voicesearch":
+                bananaUtils.launchVoiceSearch(context);
+                break;
+            case "volumepanel":
+                // already disabled when screen is OFF by Elmyra app
+                bananaUtils.toggleVolumePanel(context);
+                break;
+            case "powermenu":
+                // already disabled when screen is OFF by Elmyra app
+                bananaUtils.showPowerMenu();
+                break;
+            case "toggleqspanel":
+                // already disabled when screen is OFF by Elmyra app
+                bananaUtils.toggleQsPanel();
+                break;
+            case "togglenotificationspanel":
+                // already disabled when screen is OFF by Elmyra app
+                bananaUtils.toggleNotifications();
+                break;
+            case "clearallnotifications":
+                // already disabled when screen is OFF by Elmyra app
+                bananaUtils.clearAllNotifications();
+                break;
+            case "toggleringermodes":
+                bananaUtils.toggleRingerModes(context);
+                break;
+            default:
+                break;
         }
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -133,6 +133,7 @@ import com.android.internal.logging.UiEventLoggerImpl;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.RegisterStatusBarResult;
+import com.android.internal.util.banana.LineageButtons;
 import com.android.internal.view.AppearanceRegion;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
@@ -2124,8 +2125,9 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     @Override
-    public void toggleCameraFlash() {
-        if (!isScreenFullyOff() && mDeviceInteractive && !isPulsing() && !mDozing) {
+    public void toggleCameraFlash(boolean proximityCheck) {
+        if (!proximityCheck ||
+                (!isScreenFullyOff() && mDeviceInteractive && !isPulsing() && !mDozing)) {
             toggleFlashlight();
             return;
         }
@@ -2139,6 +2141,22 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
             }
         }
+    }
+
+    // Elmyra app actions
+    @Override
+    public void triggerElmyraAction(String action) {
+        if (!isScreenFullyOff() && mDeviceInteractive && !isPulsing() && !mDozing) {
+            performTriggeredAction(action);
+            return;
+        }
+        mDozeServiceHost.triggerActionProximityCheck(action);
+    }
+
+    // Elmyra app actions
+    public void performTriggeredAction(String action) {
+        mVibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK));
+        LineageButtons.getAttachedInstance(mContext).performTriggeredAction(action, mContext, mDeviceInteractive);
     }
 
     void makeExpandedVisible(boolean force) {

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -779,7 +779,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void toggleFlashLight() {
         performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Flashlight toggle");
-        bananaUtils.toggleCameraFlash();
+        bananaUtils.toggleCameraFlash(true/*proximity check*/);
     }
 
     private UEventObserver mHDMIObserver = new UEventObserver() {